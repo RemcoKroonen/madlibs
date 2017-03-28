3efb70c8acea6fe9ddf447079789224e84f43301
Remco Kroonen <99039459@mydavinci.nl>
2017-03-28 13:49:03 +0200

    begin gemaakt

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>madlibs</title>
+	<link rel="stylesheet" type="text/css" href="madlibs.css">
+</head>
+<body>
+	<h1>madlibs</h1>
+	<form action="index_libs.php" method="get" name="madlibs">
+		<label>Welk dier zou je nooit als huisdier willen hebben?</label><input type="text" name="q1"><br>
+		<label>Wie is de belangrijkste persoon in je leven?</label><input type="text" name="q2"><br>
+		<label>In welk land zou je graag willen wonen?</label><input type="text" name="q3"><br>
+		<label>Wat doe je als je je verveelt?</label><input type="text" name="q4"><br>
+		<label>Met welk speelgoed speelde je als kind het meest?</label><input type="text" name="q5"><br>
+		<label>Bij welke docent spijbel je het liefst?</label><input type="text" name="q6"><br>
+		<label>Als je € 100.00,- had, wat zou je dan kopen?</label><input type="text" name="q7"><br>
+		<label>Wat is je fovoriete bezigheid?</label><input type="text" name="q8"><br>
+		<input type="submit" name="submit">
+	</form>
+
+
+</body>
+</html>
---
+++ index_libs.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>er heerst paniek</title>
+</head>
+<body>
+
+</body>
+</html>
