1c571f0a8fe3f6316665d28b6b93c2b3899f8f46
Remco Kroonen <99039459@mydavinci.nl>
2017-03-28 14:13:39 +0200

    verandered de eerste naar de antwoord van madlibs

--- index_libs.php
+++ index_libs.php
@@ -4,6 +4,7 @@
 	<title>er heerst paniek</title>
 </head>
 <body>
+<p>Er heerst paniek in het koninkrijk Gallifrey. Koning <?= $_GET["q6"]?> is ten einde raad en als koning <?= $_GET["q6"]?> ten einde raad is, dan roept hij zijn ten-einde-raadsheer Spinoza<br>"Spinoza! Het is een ramp! Het is een schande!"<br>"Sire, Majesteit, Uwe Luidruchtigheid, wat is er aan de hand?"<br>"Mijn <?= $_GET["q1"]?> is verdwenen! Zo maar, zonder waarschuwing.En ik had net Logo voor hem gekocht!"<br>"Majesteit uw schildpad komt vast vanzelf weer terug?"<br>"ja, da's leuk en aardig, maar hoe moet ik in de tussentijd programmeren leren?"<br>"Maar Sire, daar kunt u toch uw supercomputer voor gebruiken"<br>"Spinoza, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had."<br>"Mij vervelen, Sire."</p>
 
 </body>
 </html>
